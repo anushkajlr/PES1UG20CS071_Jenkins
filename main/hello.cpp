56a4eff8cf48b5d32a3bd2d9ebfa05d55561fe84
Anushka Jalori <79107280+anushkajlr@users.noreply.github.com>
2023-02-17 13:41:21 +0530

    Update

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES1UG20CS071 has successfully built and run\n";
-  cout << "PES1UG20CS071 has successfully CHANGED THE FILE\n";
+  cout << "File changed\n";
   return 0;
 }
